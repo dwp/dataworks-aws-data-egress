d662a4a3a5572aeb71dec520283f905afc04d5d6
Mark Stevens <mark.stevens@infinityworks.com>
2021-01-27 14:32:13 +0000

    outputting data egress server securiyt group id (#9)
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
 output "data_egress" {
   value = aws_sqs_queue.data_egress
 }
+
+output "security_group" {
+  value = {
+    data_egress_server = aws_security_group.data_egress_server.id
+  }
+}
+
