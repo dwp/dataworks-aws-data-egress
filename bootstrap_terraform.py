9980f3eff6e390c9a4637f1f6168b4c51f3d1fc9
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-04-30 13:35:38 +0100

    Replace bootstrap
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -7,50 +7,33 @@ import os
 import sys
 import yaml
 import json
-import datetime
-from dateutil.tz import tzlocal
 
 
 def main():
     if 'AWS_PROFILE' in os.environ:
         boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
-        secrets_session = boto3.Session(
-            profile_name=os.environ['AWS_PROFILE'])
-    if 'AWS_PROFILE_MGT_DEV' in os.environ:
-        secrets_session = boto3.Session(
-            profile_name=os.environ['AWS_PROFILE_MGT_DEV'])
-    elif 'AWS_SECRETS_ROLE' in os.environ:
-        secrets_session = assumed_role_session(os.environ['AWS_SECRETS_ROLE'])
     if 'AWS_REGION' in os.environ:
-        ssm = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
-        secrets_manager = secrets_session.client(
+        secrets_manager = boto3.client(
             'secretsmanager', region_name=os.environ['AWS_REGION'])
     else:
-        ssm = boto3.client('ssm')
-        secrets_manager = secrets_session.client('secretsmanager')
+        secrets_manager = boto3.client('secretsmanager')
 
     try:
-        parameter = ssm.get_parameter(
-            Name='terraform_bootstrap_config', WithDecryption=False)
-        terraform_secret = secrets_manager.get_secret_value(
+        response = secrets_manager.get_secret_value(
             SecretId="/concourse/dataworks/terraform")
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
-            print("ERROR: Invalid security token used when calling AWS SSM or Secrets Manager. Have you run `aws-sts` recently?")
+            print("ERROR: Invalid security token used when calling AWS Secrets Manager. Have you run `aws-sts` recently?")
         else:
-            print("ERROR: Problem calling AWS SSM or Secrets Manager: {}".format(
+            print("ERROR: Problem calling AWS Secrets Manager: {}".format(
                 error_message))
         sys.exit(1)
 
     config_data = yaml.load(
-        parameter['Parameter']['Value'], Loader=yaml.FullLoader)
+        response['SecretBinary'], Loader=yaml.FullLoader)
     config_data['terraform'] = json.loads(
-        terraform_secret['SecretBinary'])["terraform"]
-    config_data['opsmi'] = json.loads(
-        terraform_secret['SecretBinary'])["opsmi"]
-    config_data['data_egress'] = json.loads(
-        terraform_secret['SecretBinary'])["data_egress"]
+        response['SecretBinary'])["terraform"]
 
     with open('terraform.tf.j2') as in_template:
         template = jinja2.Template(in_template.read())
@@ -61,26 +44,6 @@ def main():
     with open('terraform.tfvars', 'w+') as terraform_tfvars:
         terraform_tfvars.write(template.render(config_data))
     print("Terraform config successfully created")
-
-
-def assumed_role_session(role_arn: str, base_session: botocore.session.Session = None):
-    base_session = base_session or boto3.session.Session()._session
-    fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
-        client_creator=base_session.create_client,
-        source_credentials=base_session.get_credentials(),
-        role_arn=role_arn,
-        extra_args={
-            #    'RoleSessionName': None # set this if you want something non-default
-        }
-    )
-    creds = botocore.credentials.DeferredRefreshableCredentials(
-        method='assume-role',
-        refresh_using=fetcher.fetch_credentials,
-        time_fetcher=lambda: datetime.datetime.now(tzlocal())
-    )
-    botocore_session = botocore.session.Session()
-    botocore_session._credentials = creds
-    return boto3.Session(botocore_session=botocore_session)
 
 
 if __name__ == "__main__":
