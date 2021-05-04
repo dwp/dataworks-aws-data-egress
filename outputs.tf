ee3be48e8fdc806a171727c5721ee37a063d0042
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-05-04 13:30:06 +0100

    SFT Agent desired capacity, environment only value (#27)
    * Output only environment specific value

--- outputs.tf
+++ outputs.tf
@@ -11,6 +11,6 @@ output "security_group" {
 output "sft_agent_service" {
   value = {
     security_group = aws_security_group.sft_agent_service.id
-    desired_count  = local.sft_agent_service_desired_count
+    desired_count  = local.sft_agent_service_desired_count[local.environment]
   }
 }
