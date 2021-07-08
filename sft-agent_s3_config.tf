5ef2ca4d0370623cf4e907da969a4e0643fba368
Mark Stevens <mark.stevens@infinityworks.com>
2021-07-08 09:22:56 +0100

    removed seperate config for production (#103)
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- sft-agent_s3_config.tf
+++ sft-agent_s3_config.tf
@@ -20,7 +20,7 @@ data "template_file" "data_egress_sft_agent_config_tpl" {
 }
 
 data "template_file" "data_egress_sft_agent_application_config_tpl" {
-  template = file("${path.module}/sft_config/${local.config_file[local.environment]}")
+  template = file("${path.module}/sft_config/${local.config_file}")
   vars = {
     destination_url = local.use_stub_nifi[local.environment] ? local.stub_nifi_alb_fqdn : data.terraform_remote_state.aws_sdx.outputs.sdx_f5_endpoint_1_name[0]
   }
