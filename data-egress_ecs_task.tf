ed93728b677a460db0f7344b9048ea983d374f61
caldwelltom <76474731+caldwelltom@users.noreply.github.com>
2021-07-30 16:16:08 +0100

    increase container memory (#117)
    Co-authored-by: thomasstock-caldwell <thomas.stock-caldwell@engineering.digital.dwp.gov.uk>

--- data-egress_ecs_task.tf
+++ data-egress_ecs_task.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_task_definition" "data-egress" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2"]
   cpu                      = "2048"
-  memory                   = "4096"
+  memory                   = "10240"
   task_role_arn            = aws_iam_role.data_egress_server_task.arn
   execution_role_arn       = data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
   container_definitions    = "[${data.template_file.data_egress_definition.rendered}, ${data.template_file.sft_agent_definition.rendered}]"
