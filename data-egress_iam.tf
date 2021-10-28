ff5a1596fbfa5b3a1e516beaf1807763990e4e36
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-10-28 15:38:30 +0100

    adding permissions to access ers files

--- data-egress_iam.tf
+++ data-egress_iam.tf
@@ -134,7 +134,8 @@ data "aws_iam_policy_document" "data_egress_server_task" {
       "${data.terraform_remote_state.common.outputs.published_bucket.arn}/businessdata/*",
       "${data.terraform_remote_state.common.outputs.published_bucket.arn}/dataegress/dwh/*",
       "${data.terraform_remote_state.common.outputs.published_bucket.arn}/dataegress/oneservice/*",
-      "${data.terraform_remote_state.common.outputs.published_bucket.arn}/common-model-inputs/*"
+      "${data.terraform_remote_state.common.outputs.published_bucket.arn}/common-model-inputs/*",
+      "${data.terraform_remote_state.common.outputs.published_bucket.arn}/dataegress/ers/*"
     ]
   }
 
