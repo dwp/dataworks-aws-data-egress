8b6ecac36000a8633efc6222c31c327738e220db
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-11-22 13:28:04 +0000

    lower specs in low envs
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- variables.tf
+++ variables.tf
@@ -12,9 +12,9 @@ variable "region" {
 variable "data_egress_server_ec2_instance_type" {
   type = map(string)
   default = {
-    development = "m5.2xlarge"
-    qa          = "m5.2xlarge"
-    integration = "m5.2xlarge"
+    development = "m5.xlarge"
+    qa          = "m5.xlarge"
+    integration = "m5.xlarge"
     preprod     = "m5.2xlarge"
     production  = "m5.2xlarge"
   }
