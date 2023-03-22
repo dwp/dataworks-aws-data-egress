5f97d6ab34ac1ee73b9b7ee17ef98f51a833c897
Jacob Baker <96300662+J-P-Baker@users.noreply.github.com>
2023-03-22 14:54:19 +0000

    database name work around
    trying a work around for name conflict issue due to using only database names

--- data-egress.tf
+++ data-egress.tf
@@ -806,7 +806,7 @@ resource "aws_dynamodb_table_item" "ers_monthly_aggregation_data_egress_config" 
   ITEM
 }
 
-resource "aws_dynamodb_table_item" "ers_qa_checks_data_egress_config" {
+resource "aws_dynamodb_table_item" "ers_qa_checks_ucr_general_data_egress_config" {
   table_name = aws_dynamodb_table.data_egress.name
   hash_key   = aws_dynamodb_table.data_egress.hash_key
   range_key  = aws_dynamodb_table.data_egress.range_key
